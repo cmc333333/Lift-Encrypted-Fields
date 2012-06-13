d2fd68344d12418841ad27d572ba5a93b21d0833
CM Lubinski <cmc333333@gmail.com>
2012-06-12 22:29:49 -0500

    Remove example class from AESField. Add OptionalAESField

--- src/main/scala/liftencryptedfields/OptionalAESField.scala
+++ src/main/scala/liftencryptedfields/OptionalAESField.scala
@@ -1,16 +1,16 @@
 package info.cmlubinski.liftencryptedfields
 
 import java.security.SecureRandom
-import net.liftweb.record.field.BinaryField
+import net.liftweb.record.field.OptionalBinaryField
 import net.liftweb.record.Record
 import net.liftweb.util.ControlHelpers.tryo
-import net.liftweb.common.Box
+import net.liftweb.common.{Box, Empty}
 import org.bouncycastle.crypto.engines.AESEngine
 import org.bouncycastle.crypto.modes.GCMBlockCipher
 import org.bouncycastle.crypto.params.{KeyParameter, ParametersWithIV}
 
 
-class AESField[OwnerType <: Record[OwnerType]](rec:OwnerType) extends BinaryField(rec) with HasKey {
+class OptionalAESField[OwnerType <: Record[OwnerType]](rec:OwnerType) extends OptionalBinaryField(rec) with HasKey {
   def binEncryptSet(plainText:Array[Byte]) = {
     val iv = new Array[Byte](32)
     new SecureRandom().nextBytes(iv)
@@ -21,23 +21,27 @@ class AESField[OwnerType <: Record[OwnerType]](rec:OwnerType) extends BinaryFiel
     val output = new Array[Byte](encrypter.getOutputSize(plainText.length))
     val cipherLength = encrypter.processBytes(plainText, 0, plainText.length, output, 0)
     encrypter.doFinal(output, cipherLength)
-    apply(Array.concat(iv, output))
+    apply(Some(Array.concat(iv, output)))
   }
-  def binDecrypt() = tryo {
-    val iv = new Array[Byte](32)
-    Array.copy(get, 0, iv, 0, 32)
-    val cipherText = new Array[Byte](get.length - 32)
-    Array.copy(get, 32, cipherText, 0, get.length - 32)
+  def binDecrypt() = get match {
+    case None => Empty
+    case Some(bin) => tryo {
+      val iv = new Array[Byte](32)
+      Array.copy(bin, 0, iv, 0, 32)
+      val cipherText = new Array[Byte](bin.length - 32)
+      Array.copy(bin, 32, cipherText, 0, bin.length - 32)
 
-    val decrypter = new GCMBlockCipher(new AESEngine())
-    decrypter.init(false, new ParametersWithIV(new KeyParameter(fieldKey), iv))
-    val output = new Array[Byte](decrypter.getOutputSize(cipherText.length))
-    val cipherLength = decrypter.processBytes(cipherText, 0, cipherText.length, output, 0)
-    decrypter.doFinal(output, cipherLength);
-    output
+      val decrypter = new GCMBlockCipher(new AESEngine())
+      decrypter.init(false, new ParametersWithIV(new KeyParameter(fieldKey), iv))
+      val output = new Array[Byte](decrypter.getOutputSize(cipherText.length))
+      val cipherLength = decrypter.processBytes(cipherText, 0, cipherText.length, output, 0)
+      decrypter.doFinal(output, cipherLength);
+      output
+    }
   }
 }
-abstract class TypedAES[FieldType, OwnerType <: Record[OwnerType]](rec:OwnerType) extends AESField[OwnerType](rec) {
+abstract class TypedOptionalAES[FieldType, OwnerType <: Record[OwnerType]](rec:OwnerType) 
+  extends OptionalAESField[OwnerType](rec) {
   def toBytes(data:FieldType):Array[Byte]
   def encryptSet(data:FieldType):OwnerType = binEncryptSet(toBytes(data))
   def <<<(data:FieldType) = encryptSet(data)
@@ -48,7 +52,8 @@ abstract class TypedAES[FieldType, OwnerType <: Record[OwnerType]](rec:OwnerType
   def decrypt_!():FieldType = decrypt.open_!
   def >>!() = decrypt_!
 }
-class AESStringField[OwnerType <: Record[OwnerType]](rec:OwnerType) extends TypedAES[String, OwnerType](rec) {
+class OptionalAESStringField[OwnerType <: Record[OwnerType]](rec:OwnerType) 
+  extends TypedOptionalAES[String, OwnerType](rec) {
   def toBytes(data:String) = data.getBytes("UTF-8")
   def fromBytes(bytes:Array[Byte]) = tryo { new String(bytes, "UTF-8") }
 }
